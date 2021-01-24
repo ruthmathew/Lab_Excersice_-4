b5e30624aa739489c4ce2b5758f48dd7b64ca504
Ruth Matewos <se.ruth.matewos@gmail.com>
2021-01-25 00:59:53 +0300

    bmi calculator done

--- Lab_Excersice_#4/BMI checker/assets/js/bmi.js
+++ Lab_Excersice_#4/BMI checker/assets/js/bmi.js
@@ -1,37 +1,61 @@
+// bmi checker object
+
 let personalProfile = {
     weight: "",
     height: "",
     bmi: 0,
 
+    // Bmi calculator function
+
     calcBmi: function(weight, height){
-        let tempBmi = Number(this.weight / Math.pow(this.height, 2)).toFixed(1)
-        return tempBmi   
+        let tempBmi = Number.parseFloat(this.weight / Math.pow(this.height, 2)).toFixed(2);
+        return tempBmi;   
     },
 
+    // bmi checker function
+
     checkBmi: function(){
-        let tempBmi = this.calcBmi()
-        let bmiValue = ""
+        let tempBmi = this.calcBmi();
+        let bmiValue = "";
 
         if(tempBmi < 18.5){
-            bmiValue = "Underweight"
+            bmiValue = "Underweight";
         }
 
         if(18.5 < tempBmi && tempBmi < 24.9){
-            bmiValue = " Normal"
+            bmiValue = " Normal";
         }
 
         if(25 < tempBmi && tempBmi < 29.9){
-            bmiValue = " Overweight"
+            bmiValue = " Overweight";
         }
 
         if(tempBmi > 30){
-            bmiValue = " Obese"
+            bmiValue = " Obese";
         }
+
+        return bmiValue;
 
         
     }
 
 }
 
+// Receive the values from input and assign to object properties
 
+personalProfile.weight = prompt("insert your weight: ");
+personalProfile.height = prompt("insert your height: ");
+
+personalProfile.bmi = personalProfile.calcBmi();
+
+
+// display the result using self Ivoking function
+
+(function(){
+    console.log("**************************************************************");
+    console.log("your Bmi value is " + personalProfile.bmi + ".");
+    console.log("your bmi status is  " + personalProfile.checkBmi() + ".");
+    console.log("**************************************************************");
+
+})();
 
